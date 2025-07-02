a9e15ef71e2db9c180f54546c435b597ac89eb71
DigiPesa2025 <pesadigi@gmail.com>
2025-07-02 10:12:18 +0300

    Configured media support

--- tech/settings.py
+++ tech/settings.py
@@ -125,6 +125,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),  # ✅ point to the main static folder
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
