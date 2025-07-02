583305d0ddcf9c053b5ea2b120ada434a0fab4af
DigiPesa2025 <pesadigi@gmail.com>
2025-07-02 11:11:47 +0300

    Added SiteContent model and admin registration

--- main/models.py
+++ main/models.py
@@ -2,3 +2,15 @@ from django.db import models
 
 class MyModel(models.Model):
     image = models.ImageField(upload_to='images/')
+
+from django.db import models
+
+class SiteContent(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    video = models.FileField(upload_to='videos/', blank=True, null=True)
+    audio = models.FileField(upload_to='audios/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from .models import SiteContent
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'main/home.html')
+    content = SiteContent.objects.last()
+    return render(request, 'main/home.html', {'content': content})
 
 def trends(request):
     return render(request, 'main/trends.html')
+
